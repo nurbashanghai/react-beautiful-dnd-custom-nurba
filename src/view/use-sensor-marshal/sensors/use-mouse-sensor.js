78cdbc68859df396f9506a20d8591b23d9022328
Azamat uulu Nurbek <nurbek@code-care.pro>
2023-10-10 18:21:00 +0600

    removed key bindings

--- src/view/use-sensor-marshal/sensors/use-mouse-sensor.js
+++ src/view/use-sensor-marshal/sensors/use-mouse-sensor.js
@@ -223,11 +223,6 @@ export default function useMouseSensor(api: SensorAPI) {
           return;
         }
 
-        // Do not start a drag if any modifier key is pressed
-        if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
-          return;
-        }
-
         const draggableId: ?DraggableId = api.findClosestDraggableId(event);
 
         if (!draggableId) {
